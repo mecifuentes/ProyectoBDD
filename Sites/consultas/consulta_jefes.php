c8e18fc5cbc286bad5df394a175875e35913a7f8
lonxo21 <alonso.venegas@uc.cl>
2021-05-28 19:11:28 -0400

    consulta2 y 6 html

--- Sites/consultas/consulta_jefes.php
+++ Sites/consultas/consulta_jefes.php
@@ -7,13 +7,11 @@
 
   $comuna = $_POST["comuna"];
   $comuna_b = strtolower($comuna);
-  if (strlen($comuna_b)>0){
  	$query = "select distinct personal.id,personal.nombre,personal.rut,personal.edad,personal.sexo from tienda,personal,despacha,comuna where tienda.jefe=personal.id and tienda.id=despacha.id_tienda and despacha.id_comuna=comuna.id and comuna.nombre like '%$comuna_b%';";
 	$result = $db -> prepare($query);
 	$result -> execute();
 	$jefes = $result -> fetchAll();
   ?>
-  if 
 	<table>
     <tr>
       <th>ID</th>
@@ -27,9 +25,6 @@
   		echo "<tr><td>$jefe[0]</td><td>$jefe[1]</td><td>$jefe[2]</td><td>$jefe[3]</td><td>$jefe[4]</td></tr>";
 	}
   ?>
-	</table>}
-  <?php if (strlen($comuna_b)==0){
-    echo "<p style="text-align:center;">No ingreso comuna, por favor intente otra vez</p>";}
-  ?>
+	</table>
 
 <?php include('../templates/footer.html'); ?>
