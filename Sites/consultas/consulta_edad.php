14c9bf781816bca8c11cb6cdc9532a50bbb204f7
lonxo21 <alonso.venegas@uc.cl>
2021-05-28 19:33:25 -0400

    consulta2 y 6 html

--- Sites/consultas/consulta_edad.php
+++ Sites/consultas/consulta_edad.php
@@ -7,7 +7,7 @@
 
   $comuna= $_POST["comuna"];
   if (strlen($comuna)==0) {
-    echo '<p style="text-align:center;">No ingresaste una comuna, te mostramos el promedio de edad de todos los trabajadores</p>';
+    echo '<p style="text-align:center;">No ingresaste una comuna, te mostramos el promedio de edad de todos los trabajadores de todas las tiendas</p>';
   }
   $query = "select avg(personal.edad) from personal,tienda,direccion,comuna where personal.tienda=tienda.id and tienda.dirección=direccion.id and direccion.comuna=comuna.id and comuna.nombre like '%$comuna%';";
   $result = $db -> prepare($query);
