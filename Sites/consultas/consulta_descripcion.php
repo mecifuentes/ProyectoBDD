081ba28ea8a9f799a2bdbc7e1bcbe485718227fd
lonxo21 <alonso.venegas@uc.cl>
2021-05-28 19:29:09 -0400

    consulta2 y 6 html

--- Sites/consultas/consulta_descripcion.php
+++ Sites/consultas/consulta_descripcion.php
@@ -7,7 +7,10 @@
 
   $descripcion = $_POST["descripcion"];
   $descripcion_b = strtolower($descripcion);
- 	$query = "select distinct usuario.id,usuario.nombre,usuario.rut,usuario.edad,usuario.sexo from usuario, compras, boleta, productos where usuario.id=compras.id_usuario AND compras.id=boleta.id_compra AND boleta.id_producto=productos.id AND productos.descripcion like '%$descripcion_b%';";
+  if (strlen($descripcion_b)==0) {
+    echo '<p style="text-align:center;">No ingresaste una descripcion, te mostramos todos los usuarios que compraron productos</p>';
+  }
+ 	$query = "select distinct usuario.id,usuario.nombre,usuario.rut,usuario.edad,usuario.sexo,productos.nombre from usuario, compras, boleta, productos where usuario.id=compras.id_usuario AND compras.id=boleta.id_compra AND boleta.id_producto=productos.id AND productos.descripcion like '%$descripcion_b%';";
 	$result = $db -> prepare($query);
 	$result -> execute();
 	$usuarios = $result -> fetchAll();
@@ -20,10 +23,11 @@
       <th>Rut</th>
       <th>Edad</th>
       <th>Sexo</th>
+      <th>Producto</th>
     </tr>
   <?php
 	foreach ($usuarios as $usuario) {
-  		echo "<tr><td>$usuario[0]</td><td>$usuario[1]</td><td>$usuario[2]</td><td>$usuario[3]</td><td>$usuario[4]</td></tr>";
+  		echo "<tr><td>$usuario[0]</td><td>$usuario[1]</td><td>$usuario[2]</td><td>$usuario[3]</td><td>$usuario[4]</td><td>$usuario[5]</td></tr>";
 	}
   ?>
 	</table>
--- Sites/consultas/consulta_edad.php
+++ Sites/consultas/consulta_edad.php
@@ -6,6 +6,9 @@
   require("../config/conexion.php");
 
   $comuna= $_POST["comuna"];
+  if (strlen($comuna)==0) {
+    echo '<p style="text-align:center;">No ingresaste una comuna, te mostramos el promedio de edad de todos los trabajadores</p>';
+  }
   $query = "select avg(personal.edad) from personal,tienda,direccion,comuna where personal.tienda=tienda.id and tienda.dirección=direccion.id and direccion.comuna=comuna.id and comuna.nombre like '%$comuna%';";
   $result = $db -> prepare($query);
   $result -> execute();
