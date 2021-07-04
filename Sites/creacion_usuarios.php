48a3fec55be567d76897ba1f51b7778a7db7c60c
lonxo21 <alonso.venegas@uc.cl>
2021-07-04 19:19:59 -0400

    test entrega 3 v2

--- Sites/creacion_usuarios.php
+++ Sites/creacion_usuarios.php
@@ -1,4 +1,4 @@
-<?php include('../templates/header.html');   ?>
+<?php include('templates/header.html');   ?>
 
 <body>
 <?php
@@ -48,4 +48,4 @@
     }
   }
   ?>
-<?php include('../templates/footer.html'); ?>
+<?php include('templates/footer.html'); ?>
