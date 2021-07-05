06d77852098b7ced1e97ff2bad7d02990a312884
lonxo21 <alonso.venegas@uc.cl>
2021-07-04 23:33:34 -0400

    login v2

--- Sites/conexion.php
+++ Sites/conexion.php
@@ -5,8 +5,8 @@
     $databaseName = 'grupo24e3';
     $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
     $user2 = 'grupo123';
-    $password2 = 'elpulentoproyecto';
-    $databaseName2 = 'grupo123e3';
+    $password2 = 'grupo123';
+    $databaseName2 = 'grupo123e2';
     $db2 = new PDO("pgsql:dbname=$databaseName2;host=localhost;port=5432;user=$user2;password=$password2");
   } catch (Exception $e) {
     echo "No se pudo conectar a la base de datos: $e";
